a10574d835b7d87c48e8e0b8f8222eca5ccb019e
chelsea-tc <chelsea.t.crawford@gmail.com>
2023-11-20 21:01:48 +0000

    hamburger menu restored and project added to sub-folder

--- react-burgers/src/App.tsx
+++ react-burgers/src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { NavBar } from "./components/navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="min-h-screen flex justify-center p-5 items-center">
+      <NavBar />
+      <div className="space-y-5 text-center gap-5">
+        <h1 className="lg:text-4xl text-2xl  font-bold">Hamburger Menu</h1>
+        <p className="max-w-lg text-sm leading-6">HAMBURGER</p>
+        <button className="px-5 py-3 bg-gradient-to-tr from-gray-100 via-gray-400 to-gray-300 rounded-md text-sm text-stone-900 hover:bg-gray-500 hover:border font-semibold">
+          Get Started
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
